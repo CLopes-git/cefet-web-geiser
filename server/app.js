6554bbe8f9575532fa5e9b10d8292b3884fd5602
CLopes-git <fbmplayer@gmail.com>
2026-05-20 10:05:54 -0300

    Exercicio 2

--- server/app.js
+++ server/app.js
@@ -1,31 +1,34 @@
 // importação de dependência(s)
 import express from 'express'
+import fs from 'fs' 
 
 // variáveis globais deste módulo
 const PORT = 3000
 const db = {}
-const app = express()  
+const app = express()
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
-
-
+const str = fs.readFileSync("server/data/jogadores.json")
+db.jogadores = (JSON.parse(str))
 
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
 //app.set('view engine', '???qual-templating-engine???');
 //app.set('views', '???caminho-ate-pasta???');
 // dica: 2 linhas
-
+app.set('view engine', 'hbs')
+app.set('views', 'server/views')
 
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
-
-
+app.get("/", function(request, response) {
+  response.render('index', db.jogadores);
+}); 
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
